9b19fc9e548f2b45e9fcff27d8556eca049d1f75
TIN-slayer <dimka_dmit@mail.ru>
2019-11-26 16:15:08 +0300

    yellow elipse

--- 0. project1.py
+++ 0. project1.py
@@ -1,8 +1,19 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('For exapmpel now')
+import pygame
 
-
-if __name__ == '__main__':
-    main()
+screen = pygame.display.set_mode((600, 600))
+screen.fill((0, 0, 255))
+running = True
+x = 0
+v = 20
+fps = 60
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            screen.fill((0, 0, 255))
+            pygame.draw.circle(screen, pygame.Color('yellow'), event.pos, 20)
+    pygame.display.flip()
+    clock.tick(50)
+pygame.quit()
